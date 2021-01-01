47ab35015d7c61f170024811c7be5bc6297dc4c2
saalik123 <63722568+saalik123@users.noreply.github.com>
2021-01-01 18:39:56 +0530

    added notification indicator to cart

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import { useSelector } from 'react-redux';
 
 
 const Header = () => {
+    const cartItems = useSelector((state) => state.cartItems);
+    const items = cartItems ? cartItems.length : undefined;
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
@@ -13,8 +16,11 @@ const Header = () => {
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="ml-auto">
-                    <LinkContainer to="/cart">
-                            <Nav.Link><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
+                    <LinkContainer to="/cart" className="cart">
+                            <Nav.Link>
+                                <i className="fas fa-shopping-cart"></i>
+                                  Cart {items && <span className="notification">{items}</span>}
+                            </Nav.Link>
                     </LinkContainer>
                     <LinkContainer to="/cart">
                             <Nav.Link><i className="fas fa-user"></i> Sign In</Nav.Link>
