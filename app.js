fa45e3ba12b27eb7ab6c9d3eff257c032206540b
saalik123 <63722568+saalik123@users.noreply.github.com>
2021-01-08 20:17:43 +0530

    added client build folder as an static folder for server

--- app.js
+++ app.js
@@ -28,6 +28,13 @@ app.use("/api/uploads", uploadRoutes)
 
 app.use("/uploads", express.static(path.join(__dirname, '/uploads')));
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '/client/build')));
+
+    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "client", "build", "index.html")));
+
+}
+
 
 const PORT = process.env.PORT || 5000;
 
